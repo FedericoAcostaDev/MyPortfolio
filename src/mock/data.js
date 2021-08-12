13ab2a8040c60f3eef6121ca31e5e7bb2d035687
FedericoAcostaDev <fedeacos045@gmail.com>
2021-08-12 18:44:42 -0300

    grammar

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,8 +19,8 @@ export const heroData = {
 export const aboutData = {
   img: 'profile.jpg',
   paragraphOne: "I'm a web developer with an art & design background.",
-  paragraphTwo: 'In other words, an Architect turned into a programer.',
-  paragraphThree: 'Especialized in front-end React.js | React Native ',
+  paragraphTwo: 'In other words, an Architect turned into a programmer.',
+  paragraphThree: 'Specialized in front-end React.js | React Native ',
   resume: '', // if no resume, the button will not show up
 };
 
